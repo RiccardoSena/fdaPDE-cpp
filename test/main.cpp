07449f8507aa7cecbb515d9abdbb826680cb64f4
beatricebianchi3 <beatrice3.bianchi@mail.polimi.it>
2024-04-03 18:38:33 +0200

    incontro palummo

--- test/main.cpp
+++ test/main.cpp
@@ -12,7 +12,7 @@
 #include "src/gcv_srpde_test.cpp"
 #include "src/gcv_qsrpde_test.cpp"
 #include "src/gcv_srpde_newton_test.cpp"
- #include "src/kcv_srpde_test.cpp"
+// #include "src/kcv_srpde_test.cpp"
 // functional test suite
 #include "src/fpca_test.cpp"
 #include "src/fpls_test.cpp"
