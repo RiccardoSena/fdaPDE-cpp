c3093ef1b6984f586db356ccc9d6ddaa74ca4758
beatricebianchi3 <beatrice3.bianchi@mail.polimi.it>
2024-05-05 10:47:31 +0200

    piccole modifiche

--- fdaPDE/models/regression/speckman.h
+++ fdaPDE/models/regression/speckman.h
@@ -66,8 +66,8 @@ template <typename Model, typename Strategy> class Speckman {
                invR0_.diagonal()[i] = 1.0 / diagElement; 
             }
             // questi non so come vadano scelti ho messo nuemri a caso ???
-            unsigned alpha = 10;    // Numero di aggiornamenti del pattern di sparsità per ogni colonna di A
-            unsigned beta = 5;      // Numero di indici da aggiungere al pattern di sparsità di Lk per ogni passo di aggiornamento
+            unsigned alpha = 20;    // Numero di aggiornamenti del pattern di sparsità per ogni colonna di A
+            unsigned beta = 7;      // Numero di indici da aggiungere al pattern di sparsità di Lk per ogni passo di aggiornamento
             double epsilon = 0.05; // Soglia di tolleranza per l'aggiornamento del pattern di sparsità
 
             DMatrix<double> tildeA_ = m.Psi().transpose()* m.Psi()+ m.lambda_D() * m.R1().transpose() * invR0_ * m.R1();
--- fdaPDE/models/regression/speckman2.h
+++ fdaPDE/models/regression/speckman2.h
@@ -46,7 +46,7 @@ template <typename Model, typename Strategy> class Speckman2: public InferenceBa
       };
 
       struct NonExactInverse{
-         DMatrix<double> compute(Model m){
+         SpMatrix<double> compute(Model m){
             return Base::invE_approx(m); 
          }
       };
